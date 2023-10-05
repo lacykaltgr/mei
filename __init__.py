0c3ae5b731c750c2dbb54d8b2241bcc963ecd5c6
lacykaltgr <freundl0509@gmail.com>
2023-10-05 15:15:58 +0200

    init_py exported

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from meitorch.mei import MEI
-from meitorch.result import MEIProcess
+import meitorch.objective as objective
+import meitorch.result as result
+import meitorch.tools as tools
 from meitorch.linearmei import LinearMEI
 
-__all__ = ['MEI', 'MEIProcess', 'LinearMEI']
+__all__ = ['MEI', 'objective', 'LinearMEI', 'result', 'tools']
